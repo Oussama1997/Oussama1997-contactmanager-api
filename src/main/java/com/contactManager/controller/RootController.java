f0cbbcf9d6b39dd5f7a30eab53e1f4ec51172d09
Oussamakbm <oussamabenmimouna.7@gmail.com>
2026-01-30 16:59:08 +0100

    Fix error

---
+++ src/main/java/com/contactManager/controller/RootController.java
@@ -0,0 +1,18 @@
+package com.contactManager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public Map<String, String> root() {
+        return Map.of(
+                "application", "Contact Manager API",
+                "status", "RUNNING"
+        );
+    }
+}
